f46376d39dab95b1435f0dea9c0c8f80b6a6a080
Viktor Khalezov <viktor-khalezov@mail.ru>
2022-03-07 23:51:38 +0300

    Внесен код 11 урока

---
+++ src/main/java/ru/gb/gbthymeleafwinter/dao/security/AuthorityDao.java
@@ -0,0 +1,10 @@
+package ru.gb.gbthymeleafwinter.dao.security;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.gb.gbthymeleafwinter.entity.security.Authority;
+
+public interface AuthorityDao extends JpaRepository<Authority, Long> {
+
+
+}
